0c616510e366ee2990393820694cac67d05ec7c4
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-04 01:18:58 +0700

    NEW: các trang tĩnh: admin/product

---
+++ src/main/java/com/example/FoodMate_Spring/controller/dashboard/ProductController.java
@@ -0,0 +1,32 @@
+package com.example.FoodMate_Spring.controller.dashboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/admin/product")
+public class ProductController {
+
+    @GetMapping(path = {"", "/", "/index"})
+    public String index() {
+        return "dashboard/product/index";
+    }
+
+    @GetMapping(path = {"/create/", "/create"})
+    public String create() {
+        return "dashboard/product/create-edit";
+    }
+
+    @GetMapping(path = {"/{id}/", "/{id}"})
+    public String show(@PathVariable int id) {
+        return "dashboard/product/show";
+    }
+
+    @GetMapping(path = {"/edit/{id}/", "/edit/{id}"})
+    public String edit(@PathVariable int id) {
+        return "dashboard/product/create-edit";
+    }
+
+}
