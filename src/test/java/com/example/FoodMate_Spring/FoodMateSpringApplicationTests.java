7a1b252424f489a87528d063d08b499ddb928a57
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-03 18:46:15 +0700

    INITITAL: FoodMate_Spring

---
+++ src/test/java/com/example/FoodMate_Spring/FoodMateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FoodMate_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
