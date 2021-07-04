f957960500c6e81007e43b40aa74fb27897c5cd7
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-04 18:09:27 +0700

    NEW: admin/product edit (chưa gồm sửa ảnh)

--- src/main/java/com/example/FoodMate_Spring/controller/dashboard/ProductController.java
+++ src/main/java/com/example/FoodMate_Spring/controller/dashboard/ProductController.java
@@ -2,13 +2,12 @@ package com.example.FoodMate_Spring.controller.dashboard;
 
 import com.example.FoodMate_Spring.model.Product;
 import com.example.FoodMate_Spring.service.product.ProductService;
+import com.example.FoodMate_Spring.service.productCategory.ProductCategoryService;
+import com.example.FoodMate_Spring.service.restaurant.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +17,10 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+    @Autowired
+    private ProductCategoryService productCategoryService;
+    @Autowired
+    private RestaurantService restaurantService;
 
     @GetMapping(path = {"", "/", "/index"})
     public String index(Model model, @RequestParam(required = false) String search) { //Có thể bỏ @RequestParam nếu dùng [required = false]
@@ -29,10 +32,12 @@ public class ProductController {
         return "dashboard/product/index";
     }
 
+
     @GetMapping(path = {"/create/", "/create"})
     public String create() {
         return "dashboard/product/create-edit";
     }
+
 
     @GetMapping(path = {"/{id}/", "/{id}"})
     public String show(Model model, @PathVariable int id) {
@@ -44,9 +49,23 @@ public class ProductController {
         return "dashboard/product/show";
     }
 
+
     @GetMapping(path = {"/{id}/edit/", "/{id}/edit"})
-    public String edit(@PathVariable int id) {
+    public String edit(Model model, @PathVariable int id) {
+
+        model.addAttribute("product", productService.findById(id));
+        model.addAttribute("productCategories", productCategoryService.findAllByOrderByIdDesc());
+        model.addAttribute("restaurants", restaurantService.findAllByOrderByIdDesc());
+
         return "dashboard/product/create-edit";
+    }
+
+    @PostMapping(path = {"/{id}/", "/{id}"})
+    public String update(@ModelAttribute Product product) {
+
+        productService.save(product);
+
+        return "redirect:/admin/product/" + product.getId();
     }
 
 }
