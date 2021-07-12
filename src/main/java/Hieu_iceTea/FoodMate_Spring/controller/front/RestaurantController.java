f53ae5d31c5fdaa17111f6035a77a918a0065fd0
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-13 01:21:19 +0700

    NEW: Front - Restaurant

--- src/main/java/Hieu_iceTea/FoodMate_Spring/controller/front/RestaurantController.java
+++ src/main/java/Hieu_iceTea/FoodMate_Spring/controller/front/RestaurantController.java
@@ -1,26 +1,56 @@
 package Hieu_iceTea.FoodMate_Spring.controller.front;
 
+import Hieu_iceTea.FoodMate_Spring.model.Restaurant;
+import Hieu_iceTea.FoodMate_Spring.service.restaurant.RestaurantService;
+import Hieu_iceTea.FoodMate_Spring.utilities.Common;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "/restaurant")
 public class RestaurantController {
 
+    //region - Autowired Service -
+    @Autowired
+    private RestaurantService restaurantService;
+
+    //endregion
+
+
+    //region - Display -
     @GetMapping(path = {"", "/", "/index"})
-    public String index() {
+    public String index(Model model, @RequestParam(required = false) String search) { //Có thể bỏ @RequestParam nếu dùng [required = false]
+
+        List<Restaurant> restaurants = restaurantService.getAll(search);
+
+        model.addAttribute("restaurants", restaurants);
 
         return "front/restaurant/index";
 
     }
 
     @GetMapping(path = {"/{id}/", "/{id}", "/{id}/{slug}.html"})
-    public String show(@PathVariable int id) {
+    public String show(Model model, @PathVariable int id, @PathVariable(required = false) String slug) {
+
+        Restaurant restaurant = restaurantService.findById(id);
+
+        if  (slug == null || slug.isBlank()) {
+            slug = Common.toSlug(restaurant.getName());
+            return "redirect:/restaurant/" + id + "/" + slug + ".html";
+        }
+
+        model.addAttribute("restaurant", restaurant);
 
         return "front/restaurant/show";
 
     }
+    //endregion
 
 }
