79ed0769fde4ac743b07810b9a2e2b393b41b00b
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-14 16:06:17 +0700

    UPDATE: Cart_Service

--- src/main/java/Hieu_iceTea/FoodMate_Spring/utilities/shoppingCart/service/implement/CartServiceImplement.java
+++ src/main/java/Hieu_iceTea/FoodMate_Spring/utilities/shoppingCart/service/implement/CartServiceImplement.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 
 @Service
-public class CartServiceImplement_List implements CartService {
+public class CartServiceImplement implements CartService {
 
     //region - Autowired -
     @Autowired
