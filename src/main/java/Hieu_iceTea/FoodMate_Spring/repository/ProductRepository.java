1e689ec79aa3d836ad6871fbb34ab39e690309b8
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-15 10:30:44 +0700

    NEW: data-rest API

--- src/main/java/Hieu_iceTea/FoodMate_Spring/repository/ProductRepository.java
+++ src/main/java/Hieu_iceTea/FoodMate_Spring/repository/ProductRepository.java
@@ -6,9 +6,11 @@ import Hieu_iceTea.FoodMate_Spring.model.Product;
 import java.util.List;
 
 
+//@RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends BaseRepository<Product, Integer> {
 
     List<Product> findAllByNameContainsOrderByIdDesc(String name);
+    //List<Product> findAllByNameContainsOrderByIdDesc(@Param("name") String name);
 
     //List<Product> findAllByNameContainsIgnoreCaseOrderByIdDesc(String name);
 
