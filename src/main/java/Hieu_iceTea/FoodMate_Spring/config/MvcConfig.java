1beb8f18555171e2646a79f228d32cbf3bd9c745
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-16 10:28:25 +0700

    NEW: MvcConfig

---
+++ src/main/java/Hieu_iceTea/FoodMate_Spring/config/MvcConfig.java
@@ -0,0 +1,15 @@
+package Hieu_iceTea.FoodMate_Spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("front/account/login");
+        registry.addViewController("/access-denied").setViewName("front/account/access-denied");
+    }
+
+}
