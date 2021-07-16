d5293d22a97177065df722a1b1638915942fa2e5
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-16 15:52:47 +0700

    NEW: front - checkout/index (Hiển thị sản phẩm giỏ hàng & thông tin đăng nhập)

--- src/main/java/Hieu_iceTea/FoodMate_Spring/controller/front/CheckOutController.java
+++ src/main/java/Hieu_iceTea/FoodMate_Spring/controller/front/CheckOutController.java
@@ -1,20 +1,46 @@
 package Hieu_iceTea.FoodMate_Spring.controller.front;
 
+import Hieu_iceTea.FoodMate_Spring.service.user.UserService;
+import Hieu_iceTea.FoodMate_Spring.utilities.shoppingCart.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping(path = "/checkout")
 public class CheckOutController {
 
+    //region - Autowired Service -
+    @Qualifier("cartServiceImplement_List")
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private UserService userService;
+    //endregion
+
+
+    //region - Display -
     @GetMapping(path = {"", "/", "/index"})
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("currentUser", userService.getCurrentUser());
+
+        model.addAttribute("carts", cartService.content());
+        model.addAttribute("total", cartService.total());
+        model.addAttribute("subtotal", cartService.subtotal());
 
         return "front/checkout/index";
 
     }
+    //endregion
 
+
+    //region - Add new -
     public String addOrder() {
 
         return "";
@@ -26,6 +52,8 @@ public class CheckOutController {
         return "";
 
     }
+    //endregion
+
 
     public String sendEmail() {
 
