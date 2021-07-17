f46c807f64a57010367199c2ce29d1c3f89699a5
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-17 12:03:12 +0700

    UPDATE: cấu hình @RequestMapping("/api")

--- src/main/java/Hieu_iceTea/FoodMate_Spring/controller/restApi/example_ThiDK/GreetingController.java
+++ src/main/java/Hieu_iceTea/FoodMate_Spring/controller/restApi/example_ThiDK/GreetingController.java
@@ -1,18 +1,20 @@
 package Hieu_iceTea.FoodMate_Spring.controller.restApi.example_ThiDK;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/api")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/api/greeting")
+    @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
