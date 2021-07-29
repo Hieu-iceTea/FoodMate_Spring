f4d1f55c09e7a6e5f3671525e9d870e679f70f09
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-29 16:35:15 +0700

    UPDATE: Chuyển hướng sau đăng nhập dùng ?continueUri=

--- src/main/java/Hieu_iceTea/FoodMate_Spring/controller/front/AccountController.java
+++ src/main/java/Hieu_iceTea/FoodMate_Spring/controller/front/AccountController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Controller
@@ -16,7 +17,9 @@ public class AccountController {
 
     //region - Account -
     @GetMapping("login")
-    public String login() {
+    public String login(HttpSession httpSession, @RequestParam(required = false) String continueUri) {
+
+        httpSession.setAttribute("continueUri", continueUri);
 
         return "front/account/login";
 
@@ -25,15 +28,19 @@ public class AccountController {
     //Nguồn: https://stackoverflow.com/questions/45709333/page-redirecting-depending-on-role-using-spring-security-and-thymeleaf-spring
     //Tham khảo cách khác tại: https://stackoverflow.com/questions/42738090/spring-role-based-redirecting-after-login
     @GetMapping("/login-success-redirect")
-    public void loginSuccessRedirect(HttpServletRequest request, HttpServletResponse response/*, Authentication authResult*/) throws IOException, ServletException {
+    public String loginSuccessRedirect(HttpServletRequest request) throws IOException, ServletException {
 
-        /*String role = authResult.getAuthorities().toString();
-        if (role.contains("ROLE_HOST") || role.contains("ROLE_ADMIN") || role.contains("ROLE_STAFF")) {*/
+        String continueUri = (String) request.getSession().getAttribute("continueUri");
+        if (continueUri != null && !continueUri.isBlank()) {
+            //request.getSession().removeAttribute("continueUri");
+
+            return "redirect:/" + continueUri;
+        }
 
         if (request.isUserInRole("HOST") || request.isUserInRole("ADMIN") || request.isUserInRole("STAFF")) {
-            response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/admin"));
+            return "redirect:/admin";
         } else {
-            response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/"));
+            return "redirect:/";
         }
 
     }
