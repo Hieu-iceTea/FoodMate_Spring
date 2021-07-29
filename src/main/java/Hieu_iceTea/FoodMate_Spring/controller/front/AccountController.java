bdc42395f648810c96deef71a93f825c77d5e9c7
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-29 14:19:59 +0700

    NEW: loginSuccessRedirect

--- src/main/java/Hieu_iceTea/FoodMate_Spring/controller/front/AccountController.java
+++ src/main/java/Hieu_iceTea/FoodMate_Spring/controller/front/AccountController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Controller
 @RequestMapping(path = "/account")
 public class AccountController {
@@ -14,6 +19,22 @@ public class AccountController {
     public String login() {
 
         return "front/account/login";
+
+    }
+
+    //Nguồn: https://stackoverflow.com/questions/45709333/page-redirecting-depending-on-role-using-spring-security-and-thymeleaf-spring
+    //Tham khảo cách khác tại: https://stackoverflow.com/questions/42738090/spring-role-based-redirecting-after-login
+    @GetMapping("/login-success-redirect")
+    public void loginSuccessRedirect(HttpServletRequest request, HttpServletResponse response/*, Authentication authResult*/) throws IOException, ServletException {
+
+        /*String role = authResult.getAuthorities().toString();
+        if (role.contains("ROLE_HOST") || role.contains("ROLE_ADMIN") || role.contains("ROLE_STAFF")) {*/
+
+        if (request.isUserInRole("HOST") || request.isUserInRole("ADMIN") || request.isUserInRole("STAFF")) {
+            response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/admin"));
+        } else {
+            response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/"));
+        }
 
     }
 
